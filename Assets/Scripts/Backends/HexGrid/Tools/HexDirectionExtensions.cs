ebf1973b879bdd8035687a4dc03c37fdf9aedb27
GaryAu83GitHub <gary.au.83.ga@gmail.com>
2022-09-12 13:36:40 +0200

    Cell Neighbors
    Set the neighbors for a hex cell

---
+++ Assets/Scripts/Backends/HexGrid/Tools/HexDirectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Backends.HexGrid.Tools
+{
+    public enum HexDirection { NE, E, SE, SW, W, NW }
+
+    public static class HexDirectionExtensions
+    {
+        public static HexDirection Opposite(this HexDirection aDir)
+        {
+            return (int)aDir < 3 ? (aDir + 3 ) : (aDir - 3);
+        }
+    }
+}
--- Assets/Scripts/Frontends/Prefabs/HexCell.cs
+++ Assets/Scripts/Frontends/Prefabs/HexCell.cs
@@ -2,10 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts.Backends.HexGrid;
+using Assets.Scripts.Backends.HexGrid.Tools;
+
 
 public class HexCell : MonoBehaviour
 {
     public HexCoordinates Coordinates;
 
     public Color Color;
+
+    [SerializeField]
+    HexCell[] Neighbors;
+
+    public HexCell GetNeighbor(HexDirection aDir)
+    {
+        return Neighbors[(int)aDir];
+    }
+
+    public void SetNeighbor(HexDirection aDir, HexCell aCell)
+    {
+        Neighbors[(int)aDir] = aCell;
+        aCell.Neighbors[(int)aDir.Opposite()] = this;
+    }
 }
