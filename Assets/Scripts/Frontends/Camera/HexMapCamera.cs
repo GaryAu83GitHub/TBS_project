15deeee9c3c10485cb9938fd81a5df943a3aed59
GaryAu83GitHub <gary.au.83.ga@gmail.com>
2023-03-28 20:19:53 +0200

    Camera movement in

--- Assets/Scripts/Frontends/Camera/HexMapCamera.cs
+++ Assets/Scripts/Frontends/Camera/HexMapCamera.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts.Backends.HexGrid;
 
 public class HexMapCamera : MonoBehaviour
 {
-    public float StickMinZoom, StickMaxZoom;
-    public float SwivelMinZoom, SwivelMaxZoom;
+    [SerializeField]
+    private float StickMinZoom, StickMaxZoom;
+    
+    [SerializeField] 
+    private float SwivelMinZoom, SwivelMaxZoom;
+    
+    [SerializeField]
+    private float MoveSpeedMinZoom, MoveSpeedMaxZoom;
+
+    [SerializeField]
+    private HexGrid Grid;
 
     private Transform mySwivel;
     private Transform myStick;
@@ -26,6 +36,11 @@ public class HexMapCamera : MonoBehaviour
         {
             AdjustZoom(zoomDelta);
         }
+
+        float xDelta = Input.GetAxis("Horizontal");
+        float zDelta = Input.GetAxis("Vertical");
+        if (xDelta != 0f || zDelta != 0f)
+            AdjustPosition(xDelta, zDelta);
     }
 
     private void AdjustZoom(float delta)
@@ -37,5 +52,27 @@ public class HexMapCamera : MonoBehaviour
 
         float angle = Mathf.Lerp(SwivelMinZoom, SwivelMaxZoom, myZoom);
         mySwivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
+    }
+
+    private void AdjustPosition(float xDelta, float zDelta)
+    {
+        Vector3 direction = new Vector3(xDelta, 0f, zDelta).normalized;
+        float damping = Mathf.Max(Mathf.Abs(xDelta), Mathf.Abs(zDelta));
+        float distance = Mathf.Lerp(MoveSpeedMinZoom, MoveSpeedMaxZoom, myZoom) * damping * Time.deltaTime;
+
+        Vector3 position = transform.localPosition;
+        position += direction * distance;
+        transform.localPosition = ClampPosition(position);
+    }
+
+    private Vector3 ClampPosition(Vector3 position)
+    {
+        float xMax = (Grid.ChunkCountX * HexMetrics.ChunkSizeX - .5f) * (2f * HexMetrics.InnerRadius);
+        position.x = Mathf.Clamp(position.x, 0f, xMax);
+
+        float zMax = (Grid.ChunkCountZ * HexMetrics.ChunkSizeZ - 1f) * (2f * HexMetrics.OuterRadius);
+        position.z = Mathf.Clamp(position.z, 0f, zMax);
+
+        return position;
     }
 }
