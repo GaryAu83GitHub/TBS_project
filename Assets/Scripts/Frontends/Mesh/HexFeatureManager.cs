7b433fd5dbb1076f1a5a23c0b6fdeb1b468e4b9b
GaryAu83GitHub <gary.au.83.ga@gmail.com>
2023-04-03 22:33:43 +0200

    Feature Prefab

--- Assets/Scripts/Frontends/Mesh/HexFeatureManager.cs
+++ Assets/Scripts/Frontends/Mesh/HexFeatureManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class HexFeatureManager : MonoBehaviour
 {
+    [SerializeField]
+    private Transform featurePrefab;
     public void Clear() { }
     public void Apply() { }
     public void AddFeature(Vector3 position) { }
