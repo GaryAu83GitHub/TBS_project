cd59c9bd08aaaa7f499d75efba00487a74150caf
GaryAu83GitHub <gary.au.83.ga@gmail.com>
2023-04-08 14:44:54 +0200

    1.5 Opening and Closing

--- Assets/Scripts/Frontends/UI/NewMapMenu.cs
+++ Assets/Scripts/Frontends/UI/NewMapMenu.cs
@@ -3,4 +3,14 @@ using UnityEngine;
 public class NewMapMenu : MonoBehaviour
 {
     public HexGrid hexGrid;
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
 }
