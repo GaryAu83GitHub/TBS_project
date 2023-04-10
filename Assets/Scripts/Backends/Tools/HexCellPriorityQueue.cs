470caee2c895bb7692542bd2c36ef6fbe9c7bcb0
GaryAu83GitHub <gary.au.83.ga@gmail.com>
2023-04-10 17:41:42 +0200

    4.1 Creating a Custom Queue

---
+++ Assets/Scripts/Backends/Tools/HexCellPriorityQueue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.Backends.Tools
+{
+    public class HexCellPriorityQueue
+    {
+        List<HexCell> list = new List<HexCell>();
+
+        public int Count { get { return count; } }
+
+        private int count = 0;
+
+        public void Enqueue(HexCell cell)
+        {
+            count += 1;
+        }
+
+        public HexCell Dequeue()
+        {
+            count -= 1;
+            return null;
+        }
+
+        public void Change(HexCell cell, int oldPriority)
+        { }
+
+        public void Clear()
+        {
+            list.Clear();
+            count = 0;
+        }
+    }
+}
