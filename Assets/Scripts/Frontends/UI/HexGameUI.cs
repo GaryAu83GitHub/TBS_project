0d9bdf7a55f18bdb5529bb91bf2b0a1135b372a4
GaryAu83GitHub <gary.au.83.ga@gmail.com>
2023-04-12 21:19:19 +0200

    3.3 Keeping Track of the Current Cell

--- Assets/Scripts/Frontends/UI/HexGameUI.cs
+++ Assets/Scripts/Frontends/UI/HexGameUI.cs
@@ -5,9 +5,23 @@ public class HexGameUI : MonoBehaviour
 {
     public HexGrid grid;
 
+    private HexCell currentCell;
+
     public void SetEditMode(bool toggle)
     {
         enabled = !toggle;
         grid.ShowUI(!toggle);
+    }
+
+    private bool UpdateCurrentCell()
+    {
+        HexCell cell = grid.GetCell(Camera.main.ScreenPointToRay(Input.mousePosition));
+
+        if(cell != currentCell)
+        {
+            currentCell = cell;
+            return true;
+        }
+        return false;
     }
 }
