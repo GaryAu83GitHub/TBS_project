fdfb704cc1067fb6db31c03606017bcadbfb6a81
GaryAu83GitHub <gary.au.83.ga@gmail.com>
2023-04-12 21:15:28 +0200

    3.2 Game UI

---
+++ Assets/Scripts/Frontends/UI/HexGameUI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexGameUI : MonoBehaviour
+{
+    public HexGrid grid;
+
+    public void SetEditMode(bool toggle)
+    {
+        enabled = !toggle;
+        grid.ShowUI(!toggle);
+    }
+}
