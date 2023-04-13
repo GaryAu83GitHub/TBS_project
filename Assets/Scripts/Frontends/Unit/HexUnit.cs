255a2571027ab1be18c26ee30840233d663c10f8
GaryAu83GitHub <gary.au.83.ga@gmail.com>
2023-04-13 22:53:04 +0200

    3.2 Looking at Something

--- Assets/Scripts/Frontends/Unit/HexUnit.cs
+++ Assets/Scripts/Frontends/Unit/HexUnit.cs
@@ -10,6 +10,7 @@ public class HexUnit : MonoBehaviour
     public static HexUnit unitPrefab;
 
     private const float travelSpeed = 4f;
+    private const float rotationSpeed = 180f;
 
     public HexCell Location
     {
@@ -114,6 +115,8 @@ public class HexUnit : MonoBehaviour
     private IEnumerator TravelPath()
     {
         Vector3 a, b, c = pathToTravel[0].Position;
+        transform.localPosition = c;
+        yield return LookAt(pathToTravel[1].Position);
 
         float t = Time.deltaTime * travelSpeed;
         for (int i = 1; i < pathToTravel.Count;i++)
@@ -147,6 +150,29 @@ public class HexUnit : MonoBehaviour
         }
 
         transform.localPosition = location.Position;
+        orientation = transform.localRotation.eulerAngles.y;
+    }
+
+    private IEnumerator LookAt(Vector3 point)
+    {
+        point.y = transform.localPosition.y;
+
+        Quaternion fromRotation = transform.localRotation;
+        Quaternion toRotation = Quaternion.LookRotation(point - transform.localPosition);
+
+        float angle = Quaternion.Angle(fromRotation, toRotation);
+
+        if (angle > 0f)
+        {
+            float speed = rotationSpeed / angle;
+            for (float t = Time.deltaTime * speed; t < 1f; t += Time.deltaTime * speed)
+            {
+                transform.localRotation = Quaternion.Slerp(fromRotation, toRotation, t);
+                yield return null;
+            }
+        }
+
+        transform.LookAt(point);
         orientation = transform.localRotation.eulerAngles.y;
     }
 }
